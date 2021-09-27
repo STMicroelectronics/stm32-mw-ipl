aa535a35d02287f0b874f997b50329961c8c6c32
Claudio Marchisio <claudio.marchisio@st.com>
2021-09-27 13:39:37 +0200

    Package component for FOSS-out
    - Update files created by to use the updated header template
    - Add SLA0044 LICENSE.txt, overriding MIT license
    - Update Release Notes content for public release, removing internal
      notes
    - Update Release Notes to updated template

---
+++ Release_Notes.md
@@ -0,0 +1,42 @@
+---
+pagetitle: Release Notes for STM32 Image Processing Library
+lang: en
+header-includes: <link rel="icon" type="image/x-icon" href="_htmresc/favicon.png" />
+---
+
+::: {.row}
+::: {.col-sm-12 .col-lg-4}
+
+<center>
+# Release Notes for STM32 Image Processing Library
+Copyright &copy; 2021 STMicroelectronics\
+
+[![ST logo](_htmresc/st_logo.png)](https://www.st.com){.logo}
+</center>
+
+# Purpose
+
+The STM32 Image Processing Library is a C software library for STM32 MCUs
+providing specific functionalities that help the development of visual analysis
+applications.
+:::
+
+::: {.col-sm-12 .col-lg-8}
+# Update history
+
+
+::: {.collapse}
+<input type="checkbox" id="collapse-section1" aria-hidden="true">
+<label for="collapse-section1" aria-hidden="true">__Version / Date__</label>
+<div>
+
+Initial version.
+
+- Based on [OpenMV](https://github.com/openmv/openmv) v3.6.9 `f79167de4b`
+
+</div>
+:::
+
+:::
+:::
+
--- Src/stm32ipl_object_det.c
+++ Src/stm32ipl_object_det.c
@@ -6,18 +6,18 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
- * All rights reserved.</center></h2>
+ * Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.
  *
  * Portions of this file is part of the OpenMV project.
  *
  * Copyright (c) 2013-2019 Ibrahim Abdelkader <iabdalkader@openmv.io>
  * Copyright (c) 2013-2019 Kwabena W. Agyeman <kwagyeman@openmv.io>
  *
- * This software component is licensed under MIT License, the "License";
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *                        opensource.org/licenses/MIT
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
  *
  ******************************************************************************
  */
